2c1f91ff535d1466e1f8efe78886729596631a6e
achmand <dylan.vassallo@hotmail.com>
2019-06-09 23:57:53 +0200

    [CODE] Adding cloud .sh

--- src/scripts/cloud_flink_topics.sh
+++ src/scripts/cloud_flink_topics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/delinvas/repos/flink/build-target/bin/flink \
+flink run -p 8 \
 run -c batch.ScipiBatchTopics $1/src/scipi/jars/scipi_topics.jar \
 --cassandra_point ec2-18-195-41-253.eu-central-1.compute.amazonaws.com \
 --n_occurrences $2 \
