687ffe6a21bd1519535dc1cdd200e8958da46c29
achmand <dylan.vassallo@hotmail.com>
2019-06-10 03:48:25 +0200

    Code adding Jar

--- src/scripts/cloud_flink_association.sh
+++ src/scripts/cloud_flink_association.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flink run -p 8 $1/src/scipi/jars/scipi_association.jar \
+flink run -p 8 -c batch.ScipiBatchAssociation $1/src/scipi/jars/scipi_association.jar \
 --cassandra_point ec2-18-195-41-253.eu-central-1.compute.amazonaws.com \
 --keywords "$2" \
 --results_path "$3" \
